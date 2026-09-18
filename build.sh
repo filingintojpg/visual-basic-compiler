e6a7aa675f87633f3601b7ced196f941cee8bbde
Ivan Maltsev <filingintojpg@gmail.com>
2026-09-18 22:18:38 +0300

    chore: Add CMake to the project

--- build.sh
+++ build.sh
@@ -1,32 +1,8 @@
 #!/bin/bash
-
 set -e
 
-BUILD_DIR="/build"
-LEXER_CPP="$BUILD_DIR/lexer.cpp"
-OUTPUT="$BUILD_DIR/VisualBasicCompiler"
+cmake -S /app -B /build
+cmake --build /build
 
-mkdir -p "$BUILD_DIR"
-
-echo "Generating lexer..."
-
-flex --c++ \
-    -o "$LEXER_CPP" \
-    /app/lexer.l
-
-echo "Compiling..."
-
-SOURCES=$(find /app -name "*.cpp" -not -path "*/Debug/*" -not -path "*/Release/*")
-
-g++ -std=c++20 \
-    -I/app \
-    -I/usr/include \
-    $SOURCES \
-    "$LEXER_CPP" \
-    -o "$OUTPUT"
-
-echo "Build successful!"
-echo "Running..."
 echo ""
-
-"$OUTPUT" "$@"
+/build/VisualBasicCompiler "$@"
